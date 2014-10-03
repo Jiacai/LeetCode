219c0fa01dc5b389a01f3b85b8ff0802c3f632f0
Jiacai <sturgle@hotmail.com>
2014-10-03 10:46:48 +0800

    a simpler solution

--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
-    void solve(vector<vector<char>> &board) {
-        int m = board.size();
-        if (m == 0) return;
-        int n = board[0].size();
-        if (n == 0) return;
-        for (int i = 0; i < n; i++)
-        {
-            update(board, 0, i);
-        }
-        for (int i = 1; i < m; i++)
-        {
-            update(board, i, n-1);
-        }
-        for (int i = n-2; i >= 0; i--)
-        {
-            update(board, m-1, i);
-        }
-        for (int i = m-2; i >=1; i--)
-        {
-            update(board, i, 0);
-        }
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (board[i][j]=='#') board[i][j] = 'O';
-                else if (board[i][j] == 'O') board[i][j] = 'X';
-            }
-        }
-    }
-     
-    void update(vector<vector<char>> &board, int i, int j)
+    int N;
+    int M;
+    void solve(vector<vector<char>> &board)
     {
-        if (i >= board.size() || j >= board[0].size()) return;
-        if (board[i][j] == 'O')
-        {
-            board[i][j] = '#';
-            update(board, i-1, j);
-            update(board, i+1, j);
-            update(board, i, j-1);
-            update(board, i, j+1);
-        }
+        if (board.empty() || board[0].empty()) return;
+        N = board.size();
+        M = board[0].size();
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+            {
+                if (i == 0 || j == 0 || i == N-1 || j == M-1)
+                    probe(board, i, j);
+            }
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+            {
+                if (board[i][j] != '.')
+                    board[i][j] = 'X';
+                else
+                    board[i][j] = 'O';
+            }
+    }
+ 
+    void probe(vector<vector<char>> &board, int i, int j)
+    {
+        if (i < 0 || i >= N) return;
+        if (j < 0 || j >= M) return;
+        if (board[i][j] != 'O') return;
+        board[i][j] = '.';
+        probe(board, i-1, j);
+        probe(board, i+1, j);
+        probe(board, i, j-1);
+        probe(board, i, j+1);
     }
 };
