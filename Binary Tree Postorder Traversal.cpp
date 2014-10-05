651e42b83396352568bfbda3c9b7af07a77f1e46
Jiacai <sturgle@hotmail.com>
2014-10-05 21:10:03 +0800

    add a new way - to check with count

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
+        postorderTraversal_1(root);
+    }
+    
+    vector<int> postorderTraversal_1(TreeNode *root) {
         vector<int> ans;
         if (root == NULL) return ans;
         stack<TreeNode*> st;
@@ -28,5 +32,27 @@ public:
             prev = current;
         }
         return ans;
+    }
+    
+    vector<int> postorderTraversal_2(TreeNode *root) {
+        vector<int> vec;
+        stack<pair<TreeNode*, int>> stk;
+        TreeNode* node = root;
+        while (not stk.empty() || node != NULL) {
+            if (node != NULL) {
+                stk.push(make_pair(node, 1));
+                node = node->left;
+            } else {
+                if (stk.top().second == 1) {
+                    stk.top().second = 2;
+                    node = stk.top().first->right;
+                } else {
+                    vec.push_back(stk.top().first->val);
+                    stk.pop();
+                    node = NULL;
+                }
+            }
+        }
+        return vec;
     }
 };
