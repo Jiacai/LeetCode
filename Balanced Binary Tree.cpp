cd9732d32450ee4c0ba3b1c04eb282cf5b071cd9
Jiacai <sturgle@hotmail.com>
2014-10-06 10:44:08 +0800

    Update Balanced Binary Tree.cpp

--- Balanced Binary Tree.cpp
+++ Balanced Binary Tree.cpp
@@ -1,3 +1,7 @@
+/**
+ * Annie's solution is more consistent
+ */ 
+
 class Solution {
 public:
     bool isBalanced(TreeNode *root) {
@@ -16,5 +20,22 @@ public:
             balanced = false;
         };
         return height;
+    }
+    
+    bool isBalanced_1(TreeNode *root) {
+        int height = 0;
+        return isBalancedRe(root, height);
+    }
+    
+    bool isBalancedRe(TreeNode *root, int &height){
+        if (!root) return true;
+        
+        int leftHeight = 0, rightHeight = 0;
+        if (!isBalancedRe(root->left, leftHeight)) return false;
+        if (!isBalancedRe(root->right, rightHeight)) return false;
+        if (abs(leftHeight-rightHeight) > 1) return false;
+        
+        height = 1 + max(leftHeight, rightHeight);
+        return true;
     }
 };
