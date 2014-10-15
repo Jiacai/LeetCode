260769abc21bc266b19deacf7b48bdecf7fc399c
Jiacai <sturgle@hotmail.com>
2014-10-15 10:28:44 +0800

    Create Edit Distance.cpp

---
+++ Edit Distance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int> > dp;
+        int m = word1.size();
+        int n = word2.size();
+        dp.resize(m+1);
+        for (int i = 0; i <= m; i++) {
+            dp[i].resize(n+1);
+        }
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
+                    dp[i][j] = min(dp[i][j], dp[i-1][j-1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
