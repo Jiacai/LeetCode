90f6234defcc4ba650d65df586537a3adc6f1fa0
Jiacai <sturgle@hotmail.com>
2014-10-16 12:13:57 +0800

    Update Reorder List.cpp

--- Reorder List.cpp
+++ Reorder List.cpp
@@ -20,6 +20,7 @@ public:
         delete dummy;
         return prev;
     }
+    
     void reorderList(ListNode *head) {
         if (head == NULL) return;
         ListNode *fast = head;
@@ -46,4 +47,34 @@ public:
             second->next = NULL;
         }
     }    
+    
+    void reorderList_2(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return;
+        ListNode *fast = head->next;
+        ListNode *slow = head;
+        // fast will finally stop at an even position 1->2->3->4
+        while (fast!= NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode *mid = slow;
+        ListNode *cur = mid->next;
+        while (cur->next) {
+            ListNode *mov = cur->next;
+            cur->next = mov->next;
+            mov->next = mid->next;
+            mid->next = mov;
+        }
+        ListNode *p2 = mid->next;
+        mid->next = NULL;
+        ListNode *p1 = head;
+        while (p1 && p2) {
+            ListNode *tmp = p2->next;
+            p2->next = p1->next;
+            p1->next = p2;
+            p1 = p2->next;
+            p2 = tmp;
+        }
+    }
 };
