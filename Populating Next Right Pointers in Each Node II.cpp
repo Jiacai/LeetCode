c71736fa7ed3f79ff0d900f76886358faa7e9216
Jiacai <sturgle@hotmail.com>
2014-10-20 23:14:38 +0800

    Update Populating Next Right Pointers in Each Node II.cpp

--- Populating Next Right Pointers in Each Node II.cpp
+++ Populating Next Right Pointers in Each Node II.cpp
@@ -6,33 +6,33 @@ public:
     void connect(TreeLinkNode *root) {
         if (root == NULL) return;
         queue<TreeLinkNode*> que;
-		que.push(root);
-		int curLevel = 1;
-		int nextLevel = 0;
-		TreeLinkNode* dummy = new TreeLinkNode(0);
-		TreeLinkNode* prev = dummy;
-		while (not que.empty()) {
-			TreeLinkNode* node = que.front();
-			que.pop();
-			if (node->left != NULL) {
-				que.push(node->left);
-				nextLevel++;
-			}
-			if (node->right != NULL) {
-				que.push(node->right);
-				nextLevel++;
-			}
-			curLevel--;
-			prev->next = node;
-			prev = node;
-			if (curLevel == 0) {
-				curLevel = nextLevel;
-				nextLevel = 0;
-				prev = dummy;
-				node->next = NULL;
-			}
-		}
-		delete dummy;
+        que.push(root);
+        int curLevel = 1;
+        int nextLevel = 0;
+        TreeLinkNode* dummy = new TreeLinkNode(0);
+        TreeLinkNode* prev = dummy;
+        while (not que.empty()) {
+            TreeLinkNode* node = que.front();
+            que.pop();
+            if (node->left != NULL) {
+                que.push(node->left);
+                nextLevel++;
+            }
+            if (node->right != NULL) {
+                que.push(node->right);
+                nextLevel++;
+            }
+            curLevel--;
+            prev->next = node;
+            prev = node;
+            if (curLevel == 0) {
+                curLevel = nextLevel;
+                nextLevel = 0;
+                prev = dummy;
+                node->next = NULL;
+            }
+        }
+        delete dummy;
     }
     
     void connect_1(TreeLinkNode *root) {
