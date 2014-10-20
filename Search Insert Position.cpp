4e5d7be9928a18cb77f4e79db40219628846fdd1
Jiacai <sturgle@hotmail.com>
2014-10-20 23:33:49 +0800

    Create Search Insert Position.cpp

---
+++ Search Insert Position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+        int left = 0;
+        int right = n - 1;
+		int mid = 0;
+        while (left <= right) {
+			mid = left + (right - left) / 2;
+			if (A[mid] == target) {
+				return mid;
+			} else if (A[mid] > target) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+        }
+        return left;
+    }
+};
