cc9010b2b0e94179b0923d362dcaf5a395c563c6
Jiacai <sturgle@hotmail.com>
2014-10-20 23:34:20 +0800

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -3,16 +3,16 @@ public:
     int searchInsert(int A[], int n, int target) {
         int left = 0;
         int right = n - 1;
-		int mid = 0;
+        int mid = 0;
         while (left <= right) {
-			mid = left + (right - left) / 2;
-			if (A[mid] == target) {
-				return mid;
-			} else if (A[mid] > target) {
-				right = mid - 1;
-			} else {
-				left = mid + 1;
-			}
+            mid = left + (right - left) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
         return left;
     }
