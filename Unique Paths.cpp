1833d53020fff639bdfa87e34b9fc0ba66a6195e
Jiacai <sturgle@hotmail.com>
2014-10-25 18:51:51 +0800

    Create Unique Paths.cpp

---
+++ Unique Paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp;
+        dp.resize(m);
+        for (int i = 0; i < m; i++) {
+            dp[i].resize(n);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                } else if (i == 0) {
+                    dp[0][j] = dp[0][j - 1];
+                } else if (j == 0) {
+                    dp[i][0] = dp[i - 1][0];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
