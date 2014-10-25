9fa2432888d6684b48783e10a1ccbed8294bba64
Jiacai <sturgle@hotmail.com>
2014-10-25 16:46:45 +0800

    Update Valid Number.cpp

--- Valid Number.cpp
+++ Valid Number.cpp
@@ -1,3 +1,6 @@
+/*
+Annie's solution uses a transition table
+*/
 class Solution {
 public:
     bool isSign(const char c) {
@@ -114,4 +117,40 @@ public:
         }
         return (state == 2 || state == 4 || state == 6 || state == 8 || state == 9);
     }
+    
+    bool isNumber_1(const char *s) {
+        enum InputType {INVALID, SPACE, SIGN, DIGIT, DOT, EXPONENT};
+        int transitionTable[][SPACEEND] = 
+        { /* 0   1   2   3   4   5  */
+             0,  1,  2,  3,  4,  0, // 0: INVALID
+             0,  1,  2,  3,  4,  0, // 1: SPACE
+             0,  0,  0,  3,  4,  0, // 2: SIGN
+             0,  6,  0,  3,  7,  5, // 3: DIGIT
+             0,  0,  0,  7,  0,  0, // 4: DOT
+             0,  0,  2,  8,  0,  0, // 5: EXPONENT
+             0,  6,  0,  0,  0,  0, // 6: END WITH SPACE
+             0,  6,  0,  7,  0,  5, // 7: DOT AND DIGIT
+             0,  6,  0,  8,  0,  0, // 8: END WITH SPACE OR DIGIT
+        };
+        
+        InputType last = INVALID;
+        while (*s != '\0')
+        {
+            InputType state = INVALID;
+            if (*s == ' ')
+                state = SPACE;
+            else if (isdigit(*s))
+                state = DIGIT;
+            else if (*s == '+' || *s == '-')
+                state = SIGN;
+            else if (*s == 'e')
+                state = EXPONENT;
+            else if (*s == '.')
+                state = DOT;
+            last = (InputType) transitionTable[last][state];
+            if (last == INVALID) return false;
+            s++;
+        }
+        bool validFinal[] = {0, 0, 0, 1, 0, 0, 1, 1, 1};
+        return val
 };
