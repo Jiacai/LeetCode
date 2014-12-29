91218f9df4b33cb3be0f7d25affb353d133334aa
Jiacai <sturgle@hotmail.com>
2014-12-29 18:00:43 +0800

    Update Reverse Nodes in k-Group.cpp

--- Reverse Nodes in k-Group.cpp
+++ Reverse Nodes in k-Group.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     ListNode *reverseKGroup(ListNode *head, int k) {
-        ListNode* p=head;
-        ListNode* pre=new ListNode(0);
-        pre->next=p;
-        head = pre;
-        int c=0;
-        ListNode* st;
-        ListNode* ed;
-          
-        while(p!=NULL){
-            c++;
-            if (c==1){st = p;} //get start node of every k nodes
-            if (c==k){
-                ed = p;         // get end node of every k nodes
-                ListNode *last=ed;  //store the list after the k nodes
-                ListNode *nst=st;   //store the next node to be reversed
-                while (st!=ed){     // reverse the k nodes
-                    last = ed->next;
-                    nst = st->next;
-                    st->next = last;
-                    ed->next = st;
-                    st=nst;
+        ListNode dummy(0);
+        ListNode* prevTail = &dummy;
+        ListNode *nextHead = head;
+        ListNode *thisHead = head;
+        bool done = false;
+        while (!done) {
+            for (int i = 0; i < k; i++) {
+                if (nextHead == NULL) {
+                    done = true;
+                    break;
                 }
-                pre->next = st;     //link to the previous list
-                for (int i=0;i<k-1;i++){    //get the end of the k nodes
-                    p=p->next;    
-                }
-                c=0;                //reset count = 0 
+                nextHead = nextHead->next;
             }
-            if (c==0){pre = p;}     //store the previous list
-            p=p->next;              //go next nodes
+            if (!done) {
+                ListNode *prev = NULL;
+                ListNode *current = thisHead;
+                while (current != nextHead) {
+                    ListNode *next = current->next;
+                    current->next = prev;
+                    prev = current;
+                    current = next;
+                }
+                prevTail->next = prev;
+                prevTail = thisHead;
+                thisHead = nextHead;
+            } else {
+                prevTail->next = thisHead;
+            }
         }
-        return head->next;
+        return dummy.next;
     }
 };
