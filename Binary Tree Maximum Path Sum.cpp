d0f931940c1699b4c6b9e652a57d7a691ed81d84
Jiacai <sturgle@hotmail.com>
2015-01-06 23:03:35 +0800

    Update Binary Tree Maximum Path Sum.cpp

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -3,19 +3,18 @@ public:
     int maxPathSum(TreeNode *root) {
         if (root == NULL) return 0;
         int result = root->val;
-		maxPathSumRe(root, result);
-		return result;
+        maxPathSumRe(root, result);
+        return result;
     }
 	
-	int maxPathSumRe(TreeNode *root, int &result) {
-        if (root == NULL) return 0;
-		int leftMax = maxPathSumRe(root->left, result);
-		int rightMax = maxPathSumRe(root->right, result);
-		int curMax = root->val;
-		curMax = max(curMax, root->val + leftMax);
-		curMax = max(curMax, root->val + rightMax);
-		result = max(result, root->val + leftMax + rightMax);
-		result = max(result, curMax);
-		return curMax;
+    int maxPathSumRe(TreeNode *root, int &result) {
+        if (root == NULL) {
+            return 0;
+        }
+        int leftMax = maxPathSumRe(root->left, result);
+        int rightMax = maxPathSumRe(root->right, result);
+        result = max(result, root->val + max(0, leftMax) + max(0, rightMax));
+        int sum = max(0, max(leftMax, rightMax)) + root->val;
+        return sum;
     }
 };
